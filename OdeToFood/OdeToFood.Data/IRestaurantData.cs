3792bb7d0d15e72e8273851cc542b8e988ad73f7
Dale Hogan <t_bone_bradley@hotmail.com>
2021-03-18 17:29:16 +1100

    Initial Commit
    Initial Commit

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,48 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        public IEnumerable<Restaurant> GetAll();
+        public Restaurant GetRestaurantsById(int id);
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Frankie's Pizza", Location = "Sydney", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Azteco's", Location = "Haberfield", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 3, Name = "Naan Stop", Location = "Leichhardt", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetRestaurantsById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
